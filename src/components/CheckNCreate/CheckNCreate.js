98ab5ee7113ec7871e32296491ceba98de27ea21
Nick Tarasov <xotabich1941@gmail.com>
2023-11-17 20:41:40 +0300

    bug fixes + add some checkbox in CheckNCreate block

--- src/components/CheckNCreate/CheckNCreate.js
+++ src/components/CheckNCreate/CheckNCreate.js
@@ -1,8 +1,10 @@
 // CheckNCreate.js
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './CheckNCreate.css';
 
 const CheckNCreate = () => {
+  const [checkboxes, setCheckboxes] = useState(Array(8).fill(false));
+
   useEffect(() => {
     const handleScroll = () => {
       const checkNCreateElement = document.getElementById('check-n-create');
@@ -25,12 +27,55 @@ const CheckNCreate = () => {
     };
   }, []);
 
+  const handleCheckboxChange = (index) => {
+    setCheckboxes((prevCheckboxes) => {
+      const newCheckboxes = [...prevCheckboxes];
+      newCheckboxes[index] = !newCheckboxes[index];
+      return newCheckboxes;
+    });
+  };
+
+  const handleButtonClick = () => {
+    // Обработка нажатия кнопки
+  };
+
+  const checkBoxTexts = ["Текст 1", "Текст 2", "Текст 3", "Текст 4", "Текст 5", "Текст 6", "Текст 7", "Текст 8"];
+
   return (
     <div id="check-n-create" className="check-n-create">
       <div className="black-bar"></div>
-      <div className="gradient-rectangle">
-        <div className="black-frame"></div>
-        <p className="text-checkn">Подобрать параметры путешествия</p>
+      <div className="checkbox-container">
+        <div className="column">
+          {[0, 1, 2, 3].map((row) => (
+            <label key={row} className="checkbox-label">
+              <input
+                id={"0" + row}
+                type="checkbox"
+                checked={checkboxes[row]}
+                onChange={() => handleCheckboxChange(row)}
+              />
+              <div className="custom-checkbox"></div>
+              <span className="checkbox-text">{checkBoxTexts[row]}</span>
+            </label>
+          ))}
+        </div>
+        <div className="column">
+          {[4, 5, 6, 7].map((row) => (
+            <label key={row} className="checkbox-label">
+              <input
+                id={"0" + row}
+                type="checkbox"
+                checked={checkboxes[row]}
+                onChange={() => handleCheckboxChange(row)}
+              />
+              <div className="custom-checkbox"></div>
+              <span className="checkbox-text">{checkBoxTexts[row]}</span>
+            </label>
+          ))}
+        </div>
+      </div>
+      <div className="button_container">
+        <button className="buttonContainer"onClick={handleButtonClick}>Сформировать</button>
       </div>
       <div className="black-bar2"></div>
     </div>
